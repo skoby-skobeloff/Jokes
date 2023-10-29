c5cb3572324922452717dd14d5d03a14fab93b29
Skoby <128781900+skoby-skobeloff@users.noreply.github.com>
2023-10-29 09:25:32 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,12 @@
-jokes = ["I have an inferiority complex, but it’s not a very good one.", "I told my doctor that I broke my arm in two places. He told me to stop going to those places.","What vegetable is cool, but not that cool? Rad-ish.","I was wondering why the baseball kept getting bigger and bigger, and then it hit me.","Why did the employee get fired from the calendar factory? She took a day off.","Worrying works! Case in point: 90% of the things I worry about never happen.","My teachers told me I’d never amount to much because I procrastinate. I told them, 'Just you wait!'","Why do seagulls fly over the sea? If they flew over the bay, they would be bagels.","I ordered a chicken and an egg from Amazon. I’ll let you know which comes first.","What do you call a magician who lost his magic? Ian."]
-joke = random.choice(jokes)
+jokes = [
+    "I have an inferiority complex, but it’s not a very good one.",
+    "I told my doctor that I broke my arm in two places. He told me to stop going to those places.",
+    "What vegetable is cool, but not that cool? Rad-ish.",
+    "I was wondering why the baseball kept getting bigger and bigger, and then it hit me.",
+    "Why did the employee get fired from the calendar factory? She took a day off.",
+    "Worrying works! Case in point: 90% of the things I worry about never happen.",
+    "My teachers told me I’d never amount to much because I procrastinate. I told them, 'Just you wait!'",
+    "Why do seagulls fly over the sea? If they flew over the bay, they would be bagels.",
+    "I ordered a chicken and an egg from Amazon. I’ll let you know which comes first.",
+    "What do you call a magician who lost his magic? Ian."
+]
