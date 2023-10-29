4d7d32c8184be43b5cd93602fd1b1a80d2be1a53
Skoby <128781900+skoby-skobeloff@users.noreply.github.com>
2023-10-29 09:38:01 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,5 +8,15 @@ jokes = [
     "My teachers told me I’d never amount to much because I procrastinate. I told them, 'Just you wait!'",
     "Why do seagulls fly over the sea? If they flew over the bay, they would be bagels.",
     "I ordered a chicken and an egg from Amazon. I’ll let you know which comes first.",
-    "What do you call a magician who lost his magic? Ian."
+    "What do you call a magician who lost his magic? Ian.",
+    "I broke my finger last week. On the other hand, I am OK.",
+    "I went to the doctor with a suspicious-looking mole. He told me they all look that way and I should have left it in the garden."
+    "Two men are on opposite sides of the river. The first man shouts, “How do I get to the other side of the river?” The other man yells, “You are on the other side of the river!”",
+    "Why would a pig dressed in black never get bullied? Because Batman has sworn to protect Goth-ham.",
+    "Every morning, I announce that I’m going running, but then I don’t. It’s a running joke.",
+    "Why is a swordfish’s nose 11 inches long? Because if it were 12 inches, it would be a foot.",
+    "What state is known for its small drinks? Minnesota.",
+    "What do you call a line of men waiting to get haircuts? A barberqueue.",
+    "I was going to tell a time-traveling joke, but you didn’t like it.",
+    "I can’t take my dog to the pond anymore because the ducks keep attacking him. That’s what I get for buying a pure bread dog."
 ]
